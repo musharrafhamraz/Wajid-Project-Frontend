61f3a6ba6fb20f3c1e54ec72c6ebfa85538da541
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-10-22 16:04:45 +0500

    profile screen code completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wajid_project/screens/expenses_page.dart';
 import 'package:wajid_project/screens/income_page.dart';
+import 'package:wajid_project/screens/profile_screen.dart';
 import 'package:wajid_project/screens/saving_screen.dart';
 import 'package:wajid_project/screens/splash_screen.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SavingPage(),
+      home: ProfileScreen(),
     );
   }
 }
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text('Profile'),
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        width: double.infinity,
+        color: Colors.blue,
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 80,
+                      child: ClipOval(
+                        child: Image.asset(
+                          'assets/pi.jpg',
+                          fit: BoxFit.cover,
+                          width: 200,
+                          height: 200,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Wajid Ali',
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+                child: Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Text('Name'),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text('Wajid Ali'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('Contact'),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text('12345678890'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('Occupation'),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text('Flutter Developer'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
